cd1487364bdcbb6d9964acdf52bded6ea27132c6
Charlie Sale <softwaresale01@gmail.com>
2024-10-21 12:35:22 -0400

    Document HTTP API

--- internal/codegen/endpoint.go
+++ internal/codegen/endpoint.go
@@ -9,11 +9,11 @@ type RequestValue struct {
 
 // APIEndpoint is an endpoint to call
 type APIEndpoint struct {
-	Name           string                  `json:"name"`
-	Endpoint       string                  `json:"endpoint"`
-	Method         string                  `json:"method"`
-	PathVariables  map[string]RequestValue `json:"pathVariables"`
-	RequestBody    RequestValue            `json:"requestBody"`
-	ResponseBody   RequestValue            `json:"responseBody"`
-	QueryVariables map[string]RequestValue `json:"queryVariables"`
+	Name           string                  `json:"name"`           // the name of the endpoint
+	Endpoint       string                  `json:"endpoint"`       // the URI endpoint that this request is located at
+	Method         string                  `json:"method"`         // the HTTP method that this endpoint consumes
+	PathVariables  map[string]RequestValue `json:"pathVariables"`  // a map of variables that are contained in the URI
+	RequestBody    RequestValue            `json:"requestBody"`    // the request attached to the body
+	ResponseBody   RequestValue            `json:"responseBody"`   // the type of the response body
+	QueryVariables map[string]RequestValue `json:"queryVariables"` // additional query variables append to URI
 }
