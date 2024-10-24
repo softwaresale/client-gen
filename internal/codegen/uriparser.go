94ed3dcc92091f43202657e2f38b50190d87c617
Charlie Sale <softwaresale01@gmail.com>
2024-10-24 17:03:01 -0400

    Add system for prefix to URI template parser
    Add a way to add a path prefix to the URI
    template

--- internal/codegen/uriparser.go
+++ internal/codegen/uriparser.go
@@ -10,8 +10,9 @@ import (
 type PathVariableMapper func(string) (string, error)
 
 type URITemplate struct {
-	Template  string
-	VarMapper PathVariableMapper
+	Template   string             // The actual URI template
+	VarMapper  PathVariableMapper // Determines how to swap out template variables
+	PathPrefix string             // some text to prepend to the template URI
 }
 
 // FormatTemplate takes an API endpoint URI template and expands it into a template string
@@ -30,6 +31,18 @@ func FormatTemplate(template URITemplate) (string, error) {
 
 		// replace the wholeMatch with the replacement
 		template.Template = strings.Replace(template.Template, wholeMatch, replacement, -1)
+	}
+
+	// prepend the prefix if it's present
+	if len(template.PathPrefix) > 0 {
+		printTmpl := "%s/%s"
+
+		// figure out if we need the slash
+		if template.Template[0] == '/' || template.PathPrefix[len(template.PathPrefix)-1] == '/' {
+			printTmpl = "%s%s"
+		}
+
+		template.Template = fmt.Sprintf(printTmpl, template.PathPrefix, template.Template)
 	}
 
 	return template.Template, nil
--- internal/codegen/uriparser_test.go
+++ internal/codegen/uriparser_test.go
@@ -32,3 +32,42 @@ func TestFormatTemplate_ShouldFailWhenMapperFails(t *testing.T) {
 	_, err := FormatTemplate(tmpl)
 	assert.Error(t, err)
 }
+
+func TestFormatTemplate_PrependPrefixWhenPresent(t *testing.T) {
+	tmpl := URITemplate{
+		Template:   "/hello/{{world}}",
+		VarMapper:  func(variable string) (string, error) { return variable, nil },
+		PathPrefix: "prefix",
+	}
+
+	actual, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "prefix/hello/world", actual)
+}
+
+func TestFormatTemplate_PrependPrefixWhenPresent_WithNoLeadingSlash(t *testing.T) {
+	tmpl := URITemplate{
+		Template:   "hello/{{world}}",
+		VarMapper:  func(variable string) (string, error) { return variable, nil },
+		PathPrefix: "prefix",
+	}
+
+	actual, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "prefix/hello/world", actual)
+}
+
+func TestFormatTemplate_PrependPrefixWhenPresent_WithPrefixSlash(t *testing.T) {
+	tmpl := URITemplate{
+		Template:   "hello/{{world}}",
+		VarMapper:  func(variable string) (string, error) { return variable, nil },
+		PathPrefix: "prefix/",
+	}
+
+	actual, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "prefix/hello/world", actual)
+}
