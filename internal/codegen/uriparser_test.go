0cf4fc348ab9de60f863d68b754ee8248756e4d0
Charlie Sale <softwaresale01@gmail.com>
2024-10-28 17:16:58 -0400

    Add prefix to URI template
    URI can have a prefix associated with it. If non-empty, then you can tack a template onto the beginning. This will also account for slashes.

--- internal/codegen/uriparser_test.go
+++ internal/codegen/uriparser_test.go
@@ -32,3 +32,55 @@ func TestFormatTemplate_ShouldFailWhenMapperFails(t *testing.T) {
 	_, err := FormatTemplate(tmpl)
 	assert.Error(t, err)
 }
+
+func TestFormatTemplate_ShouldPrependPrefix(t *testing.T) {
+	tmpl := URITemplate{
+		Template:  "/hello/{{world}}",
+		VarMapper: func(variable string) (string, error) { return variable, nil },
+		Prefix:    "/prefix",
+	}
+
+	formatted, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "/prefix/hello/world", formatted)
+}
+
+func TestFormatTemplate_ShouldPrependPrefix_AccountingForSlash1(t *testing.T) {
+	tmpl := URITemplate{
+		Template:  "hello/{{world}}",
+		VarMapper: func(variable string) (string, error) { return variable, nil },
+		Prefix:    "/prefix",
+	}
+
+	formatted, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "/prefix/hello/world", formatted)
+}
+
+func TestFormatTemplate_ShouldPrependPrefix_AccountingForSlash2(t *testing.T) {
+	tmpl := URITemplate{
+		Template:  "hello/{{world}}",
+		VarMapper: func(variable string) (string, error) { return variable, nil },
+		Prefix:    "/prefix/",
+	}
+
+	formatted, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "/prefix/hello/world", formatted)
+}
+
+func TestFormatTemplate_ShouldPrependPrefix_AccountingForSlash3(t *testing.T) {
+	tmpl := URITemplate{
+		Template:  "/hello/{{world}}",
+		VarMapper: func(variable string) (string, error) { return variable, nil },
+		Prefix:    "/prefix/",
+	}
+
+	formatted, err := FormatTemplate(tmpl)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "/prefix/hello/world", formatted)
+}
